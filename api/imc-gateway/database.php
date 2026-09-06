c5b52bf033588452cd8318086b7431a283247449
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-06 21:59:47 +0200

    IMC Gateway: add CORE database reader

--- api/imc-gateway/database.php
+++ api/imc-gateway/database.php
@@ -7,3 +7,9 @@ function imc_db(array $cfg, string $gw): PDO {
   $dsn = sprintf('mysql:host=%s;port=%d;dbname=%s;charset=utf8mb4', $cfg['db']['host'], $cfg['db']['port'], $db);
   return new PDO($dsn, $cfg['db']['user'], $cfg['db']['pass'], [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES=>false]);
 }
+
+function imc_core_db(array $cfg): PDO {
+  if ($cfg['db']['user'] === '') throw new RuntimeException('db_credentials_not_configured');
+  $dsn = sprintf('mysql:host=%s;port=%d;dbname=Sql1956795_1;charset=utf8mb4', $cfg['db']['host'], $cfg['db']['port']);
+  return new PDO($dsn, $cfg['db']['user'], $cfg['db']['pass'], [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES=>false]);
+}
