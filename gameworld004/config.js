d9fe8b1c4af930c8faadaed9b3531becda44b95e
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-08 19:38:14 +0200

    Use CORE identity for GW004 beta

--- gameworld004/config.js
+++ gameworld004/config.js
@@ -1,1 +1,1 @@
-window.IMC_GAME_WORLD={gameWorldId:'GW004',worldName:'WORLD LEAGUE',mode:'single_league',usesSmCountry:false};
+window.IMC_GAME_WORLD={gameWorldId:'GW004',mode:'single_league',usesSmCountry:false,publicDataEndpoint:'/api/imc-data/v1/site-ready.php'};
