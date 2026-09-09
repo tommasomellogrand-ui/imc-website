c8ec54126e9f0ab040c88c99534177f61bb30fa0
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-09 06:04:11 +0200

    Add spectator draw renderer proxy

---
+++ sensible-soccer-academy/live/draw.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+$src = __DIR__ . '/../draw/index.html';
+$html = @file_get_contents($src);
+if ($html === false) { http_response_code(500); echo 'draw_unavailable'; exit; }
+$html = str_replace('<head>', '<head><base href="../draw/">', $html);
+$html = str_replace('</body>', '<script src="../live/spectator.js?v=1"></script></body>', $html);
+echo $html;
