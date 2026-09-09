9656409c3e8d37306e6912cd39c6e843604ff254
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-09 15:06:30 +0200

    GW010: match club pool spacing to manager pool

--- sensible-soccer-academy-creation/gw-setting/crest-local-map.js
+++ sensible-soccer-academy-creation/gw-setting/crest-local-map.js
@@ -29,4 +29,7 @@
   Object.entries(clubs).forEach(([name,meta])=>{local[name]='./crests/'+meta.file;});
   window.IMC_GW010_CLUB_CODEX=clubs;
   window.IMC_CREST_OVERRIDES=Object.assign(window.IMC_CREST_OVERRIDES||{},local);
+  const spacing=document.createElement('style');
+  spacing.textContent='.matrix > .panel:last-child .pool .grid{padding-top:14px}.matrix > .panel:last-child .pool h3{margin-bottom:0}';
+  document.head.appendChild(spacing);
 })();
