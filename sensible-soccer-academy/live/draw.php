b26a0f6259a48252e31f8309c09c69aa11202e6d
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-09 06:51:06 +0200

    Refresh synchronized guest draw integration

--- sensible-soccer-academy/live/draw.php
+++ sensible-soccer-academy/live/draw.php
@@ -1,9 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
 $src = __DIR__ . '/../draw/index.html';
 $html = @file_get_contents($src);
 if ($html === false) { http_response_code(500); echo 'draw_unavailable'; exit; }
-$html = str_replace('<head>', '<head><base href="../draw/">', $html);
-$html = str_replace('</body>', '<script src="../live/spectator.js?v=1"></script></body>', $html);
+$html = str_replace('<head>', '<head><base href="../draw/"><meta name="robots" content="noindex,nofollow">', $html);
+$html = str_replace('</body>', '<script src="../live/spectator.js?v=2"></script></body>', $html);
 echo $html;
