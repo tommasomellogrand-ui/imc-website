23125db3d70b86d5e92d635e9605aad445edf012
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-09 14:42:26 +0200

    GW010: map all 24 clubs to local crest assets

---
+++ sensible-soccer-academy-creation/gw-setting/crest-local-map.js
@@ -0,0 +1,30 @@
+(()=>{
+  const base='./crests/';
+  const local={
+    'Peñarol':base+'penarol.png',
+    'Colo-Colo':base+'colo-colo.png',
+    'Millonarios':base+'millonarios.png',
+    'Bolívar':base+'bolivar.png',
+    'Olimpia':base+'olimpia.png',
+    'Alianza Lima':base+'alianza-lima.png',
+    'Santos':base+'santos.png',
+    'São Paulo':base+'sao-paulo.png',
+    'Grêmio':base+'gremio.png',
+    'Argentinos Juniors':base+'argentinos-juniors.png',
+    "Newell's Old Boys":base+'newells-old-boys.png',
+    'Vélez Sarsfield':base+'velez-sarsfield.png',
+    'Atalanta':base+'atalanta.png',
+    'Ferencváros':base+'ferencvaros.png',
+    'IFK Göteborg':base+'ifk-goteborg.png',
+    'Aberdeen':base+'aberdeen.png',
+    'Siviglia':base+'sevilla.png',
+    'West Ham United':base+'west-ham.png',
+    'Stoccarda':base+'vfb-stuttgart.png',
+    'Anderlecht':base+'anderlecht.png',
+    'Ajax':base+'ajax.png',
+    'Olympique Lyonnais':base+'lyon.png',
+    'Dinamo Zagreb':base+'dinamo-zagreb.png',
+    'Benfica':base+'benfica.png'
+  };
+  window.IMC_CREST_OVERRIDES=Object.assign(window.IMC_CREST_OVERRIDES||{},local);
+})();
