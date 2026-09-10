d0fb302758655e102a55f4e8320210cf98ebe21b
tommasomellogrand-ui <tommaso.mello.grand@gmail.com>
2026-09-10 07:18:10 +0200

    Remove legacy public gateway bootstrap dependency from master reads

--- site-assets/master/api.php
+++ site-assets/master/api.php
@@ -39,10 +39,9 @@ function matches_enrich(PDO $db,array $list): array {
 $stage='configuration';
 try {
   $root=dirname(__DIR__,2);
-  $cfg=require $root.'/api/imc-gateway/config.php';
-  require_once $root.'/api/imc-gateway/database.php';
+  $cfg=require $root.'/__imc_private_gateway/config.php';
   $stage='connection';
-  $db=imc_core_db($cfg);
+  $db=new PDO(sprintf('mysql:host=%s;port=%d;dbname=Sql1956795_1;charset=utf8mb4',$cfg['db']['host'],$cfg['db']['port']),$cfg['db']['user'],$cfg['db']['pass'],[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,PDO::ATTR_EMULATE_PREPARES=>false]);
   $stage='query';
   $results=union_table('IMC Site Results','site_result_id','game_world_id',true);
   $schedule=union_table('IMC Site Schedule','site_schedule_id','game_world_id',true);
